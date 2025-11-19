cc890a85c4dfa7199e578523d0d3afcbfde4c888
pan7poku <mez7bet@zoho.com>
2025-11-19 20:28:23 +0900

    Update tmimsc.js

--- tmimsc.js
+++ tmimsc.js
@@ -98,9 +98,33 @@ else{   const p2 = Promise.all([    fetch(url2).then((r) => r.json()).then((d) =
         <article class="twit-post099"><div class="imdb-rev-sc"><p class="imdb-rev-sc-pp"><p class="imdb-rev-sc-pp2"><a href="${gg}" target="_blank" rel="noopener noreferrer" id="twitop21" class="">${u5}</a>               ${raiting}              </p><p class="imdb-rev-sc-pp3 transdef55"></p></p><div class="imdb-rev-sc1"><div class="imdb-rev-sc2"><a href="${gg}" target="_blank" rel="noopener noreferrer" id="twitop21" class="" style="font-size:14px !important;color:#67C090 !important;">${u3}</a></div><div class="imdb-rev-sc3"></div></div></div><div class="imdb-trans-0"><div class="imdb-trans-01">このレビューを翻訳する <svg class="sv-cas-85 svg-pos2" data-slot="icon" fill="none" height="15" width="15" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">  <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"></path></svg></div><div class="imdb-trans-02 transdef55-off"><div class="imdb-trans-02-in transdef55-off"></div><p class="imdb-trans-02bb">powered by Google 翻訳</p></div></div><div class="twit-post0-body transdef55-off"><div class="twit-post0-body-trere"><div class="transdef55-off">${u11}<div id="secgen55aa2" class="genben5bb"><p class="treggen9">原文を表示</p><div id="secgen552" class="genben5"><p id="retrans903">${u7}</p><p id="retrans90">再翻訳</p><p id="retrans902"><p class="imdb-trans-02bb">powered by Google 翻訳</p></p></div></div></div></div><br/><br/><p class="twit-post5">${u6}</p></div></article>`;
     })    .catch((err) => {      console.error("翻訳エラー:", err);      return "";     });
   fetchPromises2.push(p2);}}
-Promise.all(fetchPromises).then((htmlBlocks) => {  popo53 += htmlBlocks.join("");tscrvv.find("#reviewtapz-body2bb").append("<section class='reveve895'>"+popo53+"</section>");tscrvv.fadeIn(200);ppdd.fadeIn(200);pp2.hide(); $(".errorpepe22bb").hide();});                         
-Promise.all(fetchPromises2).then((htmlBlocks2) => {  popo53bb += htmlBlocks2.join("");tscrvv.find("#reviewtapz-body2bb2").append("<div class='reveve895zz'>"+popo53bb+"</section>");tscrvv.fadeIn(200);ppdd.fadeIn(200);pp2.hide(); $(".errorpepe22bb").hide();});                         
-/* setTimeout(function() {$(".logoonon2bb").append('<a href="'+pop7gg+'" target="_blank" rel="noopener noreferrer">'+pop7gg+'</a>');}, 2000);       setTimeout(function() {  pp2.hide(); $(".errorpepe22bb").hide();}, 12000);  */ setTimeout(ontput, 300);  }  
+Promise.all([Promise.all(fetchPromises), Promise.all(fetchPromises2)])
+  .then(([htmlBlocks1, htmlBlocks2]) => {
+      // criticsレビューを挿入
+      popo53 += htmlBlocks1.join("");
+      tscrvv.find("#reviewtapz-body2bb").append("<section class='reveve895'>" + popo53 + "</section>");
+      
+      // audienceレビューを挿入
+      popo53bb += htmlBlocks2.join("");
+      tscrvv.find("#reviewtapz-body2bb2").append("<div class='reveve895zz'>" + popo53bb + "</div>");
+      
+      // 共通処理
+      tscrvv.fadeIn(200);
+      ppdd.fadeIn(200);
+      pp2.hide();
+      $(".errorpepe22bb").hide();
+
+      // fetch 完了後に ontput を呼ぶ
+      if (typeof ontput === "function") ontput();
+  })
+  .catch(err => {
+    
+  });
+
+  
+  
+/* setTimeout(function() {$(".logoonon2bb").append('<a href="'+pop7gg+'" target="_blank" rel="noopener noreferrer">'+pop7gg+'</a>');}, 2000);       setTimeout(function() {  pp2.hide(); $(".errorpepe22bb").hide();}, 12000);  */ 
+              }  
    })
   .catch(err => {
     tscrvv.append(errosm2);tscrvv.fadeIn(200); pp2.hide();    });
@@ -117,5 +141,6 @@ $(document).on('click', '.imdbbox3, .review-itt02', function(e){   if (!$(e.targ
         tomatozz();        }); 
   $(function(){    $(document).on('click', '.delreeev, .sabu777ZZ, .sabu777obZZ, .revhatpp, .revhatppTT', function(e){    if (!$(e.target).closest('.revhatpp-body').length) {const zazmm7 = $('.delreeev, .sabu777ZZ, .sabu777obZZ, .revhatpp, .revhatpp2, .revhatppTT'); zazmm7.fadeOut(200);   $('body').removeClass('sccansell'); $(".menu-dbb1, #ad-bottom-f1ads, #ad-bottom-pc0").fadeIn(200); } });   });    $(document).on('click', '#errorpepe', function(){ $(".errorpepe22").hide().empty();revimim(); $(".gg789").fadeIn(200);});       $(document).on('click', '#errorpepebb', function(){$(".errorpepe22bb").hide().empty();const ggg=$("#reviewtapz-body2bb");ggg.removeClass("stoprevv");ggg.addClass("eratto22"); tomatozz(); $(".gg789bb").fadeIn(200);}); 
                 });        
+
 
 
